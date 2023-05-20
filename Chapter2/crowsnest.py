b2782d1d67057dc899d47b46fb330520a9f7f57a
Ayda <aydafarhadi14@gmail.com>
2023-05-19 17:45:50 -0700

    Update crowsnest.py

--- Chapter2/crowsnest.py
+++ Chapter2/crowsnest.py
@@ -5,9 +5,12 @@ Date   : 2023-01-15
 Purpose: Rock the Casbah
 """
 import argparse
-# --------------------------------------------------
+# -------------------------------------------------
 def get_args():
-    """Get command-line arguments"""
+    """Get command-line arguments.
+     ArgumentParser is a class of argparse module. 
+     It has functions such as "add_argument" that receives arguments and metavar adds hint about that argument
+    """
     parser = argparse.ArgumentParser(
         description="Crow's Nest -- choose the correct article",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -17,7 +20,11 @@ def get_args():
     return parser.parse_args()
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Make a jazz noise here.
+    here we get arguments using the helper function that defined about (get_args).
+    Then extract word from argument that we captures using that function
+    Then check if first letter of that word is vowel, we use 'an' otherwise we use 'a'
+    """
     args = get_args()
     word=args.word
     article='an' if word[0].lower() in 'aeio' else 'a'
@@ -25,4 +32,8 @@ def main():
     
 # --------------------------------------------------
 if __name__ == '__main__':
+    """
+    Most of the programs should have main method to run . 
+    There could be a lot of helper function ,but suing main at the end of file could put them together and run it.
+    """
     main()
