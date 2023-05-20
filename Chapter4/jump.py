ed04a3f01b891252395d79be753c9a749151f82d
Ayda <aydafarhadi14@gmail.com>
2023-05-19 18:01:59 -0700

    Update jump.py

--- Chapter4/jump.py
+++ Chapter4/jump.py
@@ -2,6 +2,10 @@
 import argparse 
 
 def get_args():
+    """
+    1. Use argparse. we create parser object from argparse library that is imported and use its functions
+    2. Use "add_argument" function to get argument from user. Here argument is text. 
+    """
     parser=argparse.ArgumentParser(description='jump the five', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('text',help='input text', type=str,metavar='str')
     return parser.parse_args()
@@ -9,6 +13,12 @@ def get_args():
 
 
 def main():
+    """
+    we can test functionality of program using hard coded argument as shown below
+    Add encoding table.Dictionaires are like table with column names and key and values 
+    get text from aprser and encode its characters and then save them on new string
+    return newly created string
+    """
     #argv="867-5309"
     args=get_args()
     encodings={
@@ -32,4 +42,8 @@ def main():
 
 
 if __name__=='__main__':
+    """
+    main function is main part of every code that runs everyhting together. 
+    There could be alot of other dependent files but at the end main runs final program
+    """
     main()
